7ad85ce15a2f5c0e6ad6197eee770676cdbe6dcb
PrinceCodes <princecodes247@gmail.com>
2021-10-18 20:12:43 +0100

    refactor: change link style in carousel js

--- js/carousels.js
+++ js/carousels.js
@@ -13,7 +13,7 @@ let partnersSlide = new Glide(".glide", {
 
 for (let index = 0; index < 15; index++) {
   let test = `<li class="partners__item glide__slide">
-    <img src="../assets/images/partners/${index}.svg" alt="ag" />
+    <img src="assets/images/partners/${index}.svg" alt="ag" />
   </li>`;
   partners.innerHTML += test;
 }
