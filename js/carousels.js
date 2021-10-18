31b63e8d5139daf580a359a046e92dfc3e2a3b51
PrinceCodes <princecodes247@gmail.com>
2021-10-18 20:00:09 +0100

    refactor: trim spaces in carousel js

--- js/carousels.js
+++ js/carousels.js
@@ -1,26 +1,20 @@
-let partners = document.querySelector(".glide__slides")
+let partners = document.querySelector(".glide__slides");
 
-let partnersSlide = new Glide('.glide', {
-  type: 'carousel',
+let partnersSlide = new Glide(".glide", {
+  type: "carousel",
   autoplay: 20,
   startAt: 0,
   perView: 3,
-  focusAt: 1, 
+  focusAt: 1,
   gap: 10,
   animationTimingFunc: "linear",
   animationDuration: 4000,
-
-})
-
+});
 
 for (let index = 0; index < 15; index++) {
-    let test = `<li class="partners__item glide__slide">
+  let test = `<li class="partners__item glide__slide">
     <img src="../assets/images/partners/${index}.svg" alt="ag" />
-  </li>`    
-  partners.innerHTML += test
-  
+  </li>`;
+  partners.innerHTML += test;
 }
-partnersSlide.mount()
-
-
-
+partnersSlide.mount();
